bf15967778f5f79f54aecc2a984f21e31ff55e85
Kimberly Lu <kimlu4@comcast.net>
2017-07-20 17:08:02 -0700

    add js to calculate price with quantity chosen.

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,38 @@
 //business logic
-function Tickets(movie, time, age) {
+function Tickets(movie, time, age, quantity) {
   this.movie = movie;
   this.time = time;
   this.age = age;
+  this.quantity = quantity;
 }
 
-var newMovieTicket = new Tickets (inputMovie, InputTime, InputCategory);
-
-function Movie(type, price) {
-  this.type = [new release, old];
-  this.value = [3, 0];
-}
-function TimePeriod(period, price) {
-  this.period = [morning, matinee, evening];
-  this.value = [1, 0, 3];
-}
-function AgeCat(category, price) {
-  this.category = [minor, adult, senior];
-  this.value = [1, 2, 0];
-}
 var initialPrice = 10;
 
  Tickets.prototype.calcPrice = function() {
-   var finalPrice = return (initialPrice + this.movie.value + this.time.value + this.age.value) * qty;
+  return (initialPrice + this.movie + this.time + this.age) * this.quantity;
  }
 
 //UI logic
-$(document).ready() {
+$(document).ready(function() {
   $("#ticket-form").submit(function(event) {
     event.preventDefault();
-    var inputMovie = $("#title").val();
-    var inputTime = $("#time").val();
-    var inputCategory = $("#category").val();
-    var qty = $("#qty").val();
-  $("#receipt").text("")
-  })
-}
+    var inputMovie = parseInt($("#title").val());
+    var inputTime = parseInt($("#time").val());
+    var inputCategory = parseInt($("#category").val());
+    var inputQty = parseInt($("#qty").val());
+    var newMovieTicket = new Tickets(inputMovie, inputTime, inputCategory, inputQty);
+  $("#total").text("$" + newMovieTicket.calcPrice());
+});
+});
+// function Movie(type, price) {
+//   this.type = [new release, old];
+//   this.value = [3, 0];
+// }
+// function TimePeriod(period, price) {
+//   this.period = [morning, matinee, evening];
+//   this.value = [1, 0, 3];
+// }
+// function AgeCat(category, price) {
+//   this.category = [minor, adult, senior];
+//   this.value = [1, 2, 0];
+// }
